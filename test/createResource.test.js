2d166b2c95fc88a05d240ad0b71a999bd8772b8e
Max Gfeller <mg@cyon.ch>
2019-02-12 15:13:10 +0100

    Implement other test case

--- test/createResource.test.js
+++ test/createResource.test.js
@@ -58,3 +58,48 @@ test('Resource with single instance', async function () {
   await nextTick()
   expect(resource.registry.default.items.length).toBe(7)
 })
+
+test('Using controller', async function () {
+  let adapter = new TestAdapter()
+  adapter.nextResult = {
+    total: 7,
+    data: [1, 2, 3, 4, 5]
+  }
+
+  let localVue = createLocalVue()
+
+  localVue.use(Vuex)
+  localVue.use(PaginationPlugin)
+
+  let component = {
+    computed: {
+      test: createInstance('resource2', {})
+    },
+    render (h) {
+      return h('div')
+    }
+  }
+  let controller = createResource('resource2', adapter.fetchPage.bind(adapter))
+  expect(controller).toBeTruthy()
+
+  let store = new Vuex.Store({
+    state: {}
+  })
+
+  let wrapper = shallowMount(component, {
+    store,
+    localVue
+  })
+
+  expect(wrapper).toBeTruthy()
+
+  await nextTick()
+  let results = await controller.fetchRange({ page: 1, pageSize: 5 })
+  expect(results).toEqual([1, 2, 3, 4, 5])
+  adapter.nextResult = {
+    total: 7,
+    data: [6, 7]
+  }
+  results = await controller.fetchRange({ page: 2, pageSize: 5 })
+  expect(results).toEqual([6, 7])
+})
