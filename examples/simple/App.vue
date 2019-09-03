8e792f8fb4937cb5e101416a4e741341c6f16862
Max Gfeller <mg@cyon.ch>
2019-09-03 10:54:06 +0200

    Provide (very) simple example

---
+++ examples/simple/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in licenses.items" :key="item.id">{{ item.id }}</li>
+    </ul>
+    <a v-if="licenses.page > 1" @click="licenses.page -= 1">Previous page </a>
+    <a v-if="licenses.page < licenses.totalPages" @click="licenses.page++">Next page</a>
+  </div>
+</template>
+<script>
+import { createInstance } from '../../'
+
+export default {
+  computed: {
+    licenses: createInstance('licenses', {
+      page: 1,
+      pageSize: 10
+    })
+  }
+}
+</script>
