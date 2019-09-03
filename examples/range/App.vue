589adb9013cf860a9ef10177517e7f370c6cef77
Max Gfeller <mg@cyon.ch>
2019-09-03 11:01:30 +0200

    Add example for range mode

---
+++ examples/range/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in licenses.items" :key="item.id">{{ item.id }}</li>
+    </ul>
+    <a @click="licenses.pageTo++">Load more...</a>
+  </div>
+</template>
+<script>
+import { createInstance } from '../../'
+
+export default {
+  computed: {
+    licenses: createInstance('licenses', {
+      pageFrom: 1
+    })
+  }
+}
+</script>
