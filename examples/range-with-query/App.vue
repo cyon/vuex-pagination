8a5ae89dda83c8ec2236b3fa7d05797633714115
Lukas Besch <connect@lukasbesch.com>
2020-06-28 20:09:43 +0200

    Add example range-with-query

---
+++ examples/range-with-query/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input v-model="query" placeholder="Search for a license...">
+    <ul>
+      <li v-for="item in licenses.items" :key="item.id">{{ item.id }}</li>
+    </ul>
+    <a @click="licenses.pageTo++">Load more...</a>
+    <pre>
+      page:     {{ licenses.page }}
+      pageFrom: {{ licenses.pageFrom }}
+      pageTo:   {{ licenses.pageTo }}
+      items:    {{ licenses.items.length }}
+      total:    {{ licenses.total }}
+    </pre>
+  </div>
+</template>
+<script>
+import { createInstance } from '../../'
+
+export default {
+  data() {
+    return {
+      query: ''
+    }
+  },
+  computed: {
+    licenses: createInstance('licenses', {
+      pageFrom: 1,
+      args () {
+        return {
+          query: this.query,
+        }
+      },
+    })
+  }
+}
+</script>
