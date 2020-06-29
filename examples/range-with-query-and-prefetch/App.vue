2ed8a57afa6146607045fc4752647dc9189b1698
Lukas Besch <connect@lukasbesch.com>
2020-06-29 16:00:15 +0200

    Add example: Range mode with args fn and prefetch

---
+++ examples/range-with-query-and-prefetch/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input v-model="query" placeholder="Search for a license...">
+    <ul>
+      <li v-for="item in licenses.items" :key="item.id">{{ item.id }}</li>
+    </ul>
+    <a @click="licenses.pageTo++">Load more...</a>
+  </div>
+</template>
+<script>
+import { createInstance } from '../../'
+
+export default {
+  data() {
+    return {
+      query: ''
+    }
+  },
+  computed: {
+    licenses: createInstance('licenses', {
+      pageFrom: 1,
+      args () {
+        return {
+          query: this.query,
+        }
+      },
+    })
+  }
+}
+</script>
---
+++ examples/range-with-query-and-prefetch/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+import { PaginationPlugin, createResource } from '../../'
+import { fetchPage } from '../_api/api-client'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+Vue.use(PaginationPlugin)
+
+const store = new Vuex.Store({
+  strict: true
+})
+
+// Initialize resource
+createResource('licenses', fetchPage, { prefetch: true })
+
+new Vue({
+  render: h => h(App),
+  store
+}).$mount('#app')
